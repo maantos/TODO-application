fd4dc24a3879f0bfe4cf59d2fe554cadab0e70f6
maantos <antosmika@gmail.com>
2023-02-27 21:20:34 +0100

    data store unit tests added

--- pkg/data/tasks_test.go
+++ pkg/data/tasks_test.go
@@ -5,58 +5,17 @@ import (
 	"testing"
 )
 
-type StubTaskStore struct {
-	data map[TaskID]*Task
-}
-
-func (st *StubTaskStore) Create(t *Task) error {
-	if _, ok := st.data[t.ID]; ok {
-		return ErrTaskAlreadyExist
-	}
-	st.data[t.ID] = t
-	return nil
-}
-func (s *StubTaskStore) Read(id TaskID) (*Task, error) {
-	if c, ok := s.data[id]; ok {
-		return c, nil
-	}
-	return nil, ErrTaskNotFound
-}
-func (s *StubTaskStore) Update(t *Task) error {
-	_, err := s.Read(t.ID)
-	if err != nil {
-		return err
-	}
-	s.data[t.ID] = t
-	return nil
-
-}
-func (s *StubTaskStore) Delete(id TaskID) error {
-	_, err := s.Read(id)
-	if err != nil {
-		return err
-	}
-	delete(s.data, id)
-	return nil
-
-}
-func (s *StubTaskStore) List() []*Task {
-	x := []*Task{}
-
-	for _, v := range s.data {
-		x = append(x, v)
-	}
-	return x
-
-}
-
-func TestListTODOTasks(t *testing.T) {
-	t.Log("List Todo tasks")
+func TestCreateListTasks(t *testing.T) {
+	t.Log("Create and List todo tasks")
 
 	ts := NewTasksDB()
-
 	var task = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
-	ts.Create(task)
+
+	err := ts.Create(task)
+
+	if err != nil {
+		t.Errorf("=> Create failed with %v", err.Error())
+	}
 
 	want := []*Task{task}
 
@@ -65,70 +24,90 @@ func TestListTODOTasks(t *testing.T) {
 	}
 }
 
-func TestCreateTODOTasks(t *testing.T) {
-	t.Log("Create Todo tasks")
-
-	var stubTask = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
-	store := StubTaskStore{
-		map[TaskID]*Task{
-			"1": stubTask,
-		},
-	}
-	t.Run("Create new task", func(t *testing.T) {
-		var newTask = &Task{ID: "2", Done: false, Title: "New task", Description: "Todo task 2."}
-		err := store.Create(newTask)
-		if err != nil {
-			t.Errorf("=> Create failed with %v", err.Error())
-		}
-	})
-
-	t.Run("Create task, that already exist", func(t *testing.T) {
-		err := store.Create(stubTask)
-		if err != ErrTaskAlreadyExist {
-			t.Errorf("=> Create failed with %v", err.Error())
-		}
-	})
-}
-
-func TestUpdateTODOTasks(t *testing.T) {
-	t.Log("Update Todo tasks")
+func TestCreateTasksThatExist(t *testing.T) {
+	t.Log("Create Todo tasks that already exists")
 
 	ts := NewTasksDB()
+	var task = &Task{ID: "1", Done: true, Title: "test Title", Description: "Todo task 1."}
+
+	err := ts.Create(task)
+
+	if err != nil {
+		t.Errorf("=> Create failed with %v", err.Error())
+	}
+
+	err = ts.Create(task)
+
+	if err != ErrTaskAlreadyExist {
+		t.Error("=> Creating the same task should return an error!")
+	}
+}
+
+func TestCreateUpdateTasks(t *testing.T) {
+	t.Log("Create and Update task")
+
+	ts := NewTasksDB()
+
 	var task = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
 	err := ts.Create(task)
 
 	if err != nil {
 		t.Errorf("=> failed to create task: %v", err.Error())
 	}
-	var updated_task = &Task{ID: "1", Done: true, Title: "Updated test Title", Description: "Todo task 1."}
 
-	err = ts.Update(updated_task)
+	t.Run("Update task that doenst exist", func(t *testing.T) {
+		var updated_task = &Task{ID: "12", Done: true, Title: "New task", Description: "Todo task 12."}
+		err := ts.Update(updated_task)
 
-	if err != nil {
-		t.Errorf("=> failed to update task: %v", err.Error())
-	}
+		if err != ErrTaskNotFound {
+			t.Errorf("=> updating task that doesnt exist should return error")
+		}
+	})
 
+	t.Run("Update task", func(t *testing.T) {
+		var updated_task = &Task{ID: "1", Done: true, Title: "Task after update", Description: "Todo task 1."}
+
+		err := ts.Update(updated_task)
+
+		if err != nil {
+			t.Errorf("=> failed to update task: %v", err.Error())
+		}
+
+		if get, err := ts.Read(updated_task.ID); !reflect.DeepEqual(get, updated_task) || err != nil {
+			t.Errorf("=> failed to update task, got %v wanted %v", get, updated_task)
+		}
+	})
 }
 
-func TestReadTODOTasks(t *testing.T) {
+func TestAddReadTODOTasks(t *testing.T) {
 	t.Log("Delete Todo tasks")
 
 	ts := NewTasksDB()
 
-	var task = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
-	err := ts.Create(task)
-	if err != nil {
-		t.Errorf("=> failed to create task: %v", err.Error())
-	}
-	var got *Task
-	got, err = ts.Read(task.ID)
-	if err != nil {
-		t.Errorf("=> failed to read task: %v", err.Error())
-	}
+	t.Run("Get element from empty store", func(t *testing.T) {
+		elem, err := ts.Read("1")
+		if elem != nil || err != ErrTaskNotFound {
+			t.Errorf("=> Reading task that doesn't exist should failed.")
+		}
+	})
 
-	if got != task {
-		t.Errorf("Got %v wanted %v", got, task)
-	}
+	var task = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
+	t.Run("Get single element from the store", func(t *testing.T) {
+		err := ts.Create(task)
+		if err != nil {
+			t.Errorf("=> failed to create task: %v", err.Error())
+		}
+
+		var got *Task
+		got, err = ts.Read(task.ID)
+		if err != nil {
+			t.Errorf("=> failed to read task: %v", err.Error())
+		}
+
+		if !reflect.DeepEqual(got, task) {
+			t.Errorf("=> got %v wanted %v", got, task)
+		}
+	})
 
 }
 
@@ -137,15 +116,28 @@ func TestDeleteTODOTasks(t *testing.T) {
 
 	ts := NewTasksDB()
 
-	var task = &Task{ID: "1", Done: false, Title: "test Title", Description: "Todo task 1."}
-	err := ts.Create(task)
+	t.Run("Delete element that doesnt exist", func(t *testing.T) {
+		err := ts.Delete("1")
 
-	if err != nil {
-		t.Errorf("=> Create failed with %v", err.Error())
-	}
+		if err != ErrTaskNotFound {
+			t.Error("=> Missing element removal should throw an error")
+		}
 
-	err = ts.Delete(task.ID)
-	if err != nil {
-		t.Errorf("=> failed to delete task %v", err.Error())
-	}
+	})
+
+	var task = &Task{ID: "13", Done: false, Title: "To be Deleted", Description: "Task to be removed."}
+	t.Run("Delete element from the store", func(t *testing.T) {
+		err := ts.Create(task)
+
+		if err != nil {
+			t.Errorf("=> failed to create task: %v", err.Error())
+		}
+
+		err = ts.Delete("13")
+
+		if err != nil {
+			t.Errorf("=> failed to delete element: %v", err.Error())
+		}
+
+	})
 }
